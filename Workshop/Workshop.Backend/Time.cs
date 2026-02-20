cf295093fc0eeabd6e0ac72ebfcfcd0b37cea163
ksandra-04 <kasandrapimienta1118030@correo.itm.edu.co>
2026-02-19 19:58:26 -0500

    -Class representing a time with: hour, minute, second, and millisecond -Use private attributes -Constructor without parameters

---
+++ Workshop/Workshop.Backend/Time.cs
@@ -0,0 +1,21 @@
+﻿namespace Workshop.Backend;
+
+public class Time
+{
+    private int _hour;
+    private int _minute;
+    private int _second;
+    private int _millisecond;
+    //utilizo atributos privados
+
+    //Constructor sin parámetros
+    public Time()
+    {
+        _hour = 0;
+        _minute = 0;
+        _second = 0;
+        _millisecond = 0; 
+    }
+   
+
+}
